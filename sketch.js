13239b9834d550ed6a20bfb9b6e98f131b201d9a
Yuto Mori <tmori9banner@gmail.com>
2020-08-09 20:48:16 +0900

    add: naiveCount

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const H_BLOCK = 20 // ブロックの縦幅 px
 const BLOCK_NUM_W = 20; // 横方向のブロックの個数
 const BLOCK_NUM_H = 5; // 縦方向のブロックの個数
 
-const LEFT_MARGIN = 35; // 左の空白
+const LEFT_MARGIN = 60; // 左の空白
 const TOP_MARGIN = 100; // 上の空白
 
 
@@ -17,29 +17,18 @@ for(let y = 0; y < BLOCK_NUM_W; y++) {
   tbl[y] = new Array(BLOCK_NUM_H).fill(-1);
 }
 
-var tbl_cache = new Array(BLOCK_NUM_H);
-for(let y = 0; y < BLOCK_NUM_W; y++) {
-  tbl_cache[y] = new Array(BLOCK_NUM_H).fill(-1);
-}
-
 // mouse
 var mouse_count = 0;
 var over_mouse = false;
 var mouse_start = [0, 0]; // マウスのスタート位置
 var mouse_end = [0, 0]; // マウスのエンド位置
 
-var upper_left;
-var upper_right;
-var lower_left;
-var lower_right;
-
-var locked = false;
-
 var selected_x, selected_y;
 var selected_color = 0;
 
 // class palette
 var palette;
+var naiveCount;
 
 class Palette{
   constructor(margin_x, margin_y){
@@ -47,8 +36,8 @@ class Palette{
     this.text_y = margin_y;
     this.palette_x = margin_x;
     this.palette_y = margin_y + 10;
-    this.selected_x = margin_x + 165;
-    this.selected_y = margin_y;
+    this.selected_palette_x = margin_x + 165;
+    this.selected_palette_y = margin_y;
   }
 
   draw(){
@@ -62,9 +51,9 @@ class Palette{
     
     // Selected
     fill(0);
-    text("Selected", this.selected_x, this.selected_y);
+    text("Selected", this.selected_palette_x, this.selected_palette_y);
     fill(color_block[selected_color]);
-    rect(this.selected_x, this.selected_y+10, W_BLOCK, H_BLOCK);
+    rect(this.selected_palette_x, this.selected_palette_y+10, W_BLOCK, H_BLOCK);
   }
 
   click(){
@@ -73,36 +62,48 @@ class Palette{
           mouseX < this.palette_x+(i+1)*W_BLOCK &&
           this.palette_y <= mouseY &&
           mouseY < this.palette_y+H_BLOCK){
-            print(i);
+            selected_color = i;
+      }
+    }
+  }
+}
+
+class NaiveCount{
+  constructor(x, y){
+    this.count_table_x = x - 35;
+    this.count_table_y = y;
+    this.palette_x = x;
+    this.palette_y = y;
+  }
+
+  draw(){
+    // sub palette
+    for (let i=0; i<5; i++){
+      fill(color_block[i]);
+      rect(this.count_table_x, this.palette_y+H_BLOCK*i, W_BLOCK, H_BLOCK);
+    }
+
+    // count table
+    for (let i = 0; i < BLOCK_NUM_W; i++) {
+      var color_stack = [0, 0, 0, 0, 0];
+      for (let j = 0; j < BLOCK_NUM_H; j++){ // num color
+        noFill();
+        rect(this.palette_x+i*W_BLOCK, this.palette_y+j*H_BLOCK, W_BLOCK, H_BLOCK);
+        
+        if (tbl[i][j] == 0) {
+          color_stack[0] += 1
+          fill(0);
+          text(tbl[i][j], this.palette_x+i*W_BLOCK+6, this.palette_y+3, W_BLOCK, H_BLOCK);
+        }
       }
     }
   }
 }
 
 
-function mouseReleased() {
-  locked = false;
-}
-
-
 function mouseClicked(){
-  if (over_mouse){ // マウスクリック後に離した場合
-    paintBlock(upper_left, upper_right, lower_left, lower_right, tbl);
-    mouse_count += 1;
-    over_mouse = false;
-  }
-  
   // パレット
   palette.click();
-}
-
-
-function mousePressed() {
-  if (over_mouse) {
-    locked = true;
-  } else {
-    locked = false;
-  }
 }
 
 function mouseGridToBlockGrid(input_mouseX, input_mouseY){ // マウス座標からブロック座標
@@ -117,75 +118,46 @@ function mouseGridToBlockGrid(input_mouseX, input_mouseY){ // マウス座標か
   return [-1,-1];
 }
 
-function paintBlock(upper_left, upper_right, lower_left, lower_right, table){
-  // マウスの移動範囲で長方形に色塗り
-  for (let i = upper_left; i < upper_right+1; i++){
-    for (let j = lower_left; j < lower_right+1; j++){
-      table[i][j] = mouse_count;
-    }
-  }
-}
 
 function setup() {  
   color_block = [color("#FF723D"), color("#FFD334"), color("#052F83"), color("#0776EC"), color("#4FEEE7")]
   createCanvas(500, 600);
-  palette = new Palette(35, 40);
+  palette = new Palette(60, 40);
+  naiveCount = new NaiveCount(60, 400);
 }
 
 function draw() {
-  //print(mouse_start, mouse_end);
-  //print(tbl); 
-  //print(mouse_count%5);
-  if (mouseIsPressed){
-    [selected_x,selected_y] = mouseGridToBlockGrid(mouseX, mouseY);
-    if (selected_x != -1){
-      tbl[selected_x][selected_y] = 10;
-    }
-  }
-  
-  //print(mouseGridToBlockGrid(mouseX, mouseY));
-  //print(locked);
   background(220);
   strokeWeight(0.4);
 
   palette.draw();
-  
+  naiveCount.draw();
+
+  // point table
+  if (mouseIsPressed){
+    [selected_x,selected_y] = mouseGridToBlockGrid(mouseX, mouseY);
+    if (selected_x != -1){
+      tbl[selected_x][selected_y] = selected_color;
+    }
+  }
+
+  // paint block
   for (let i = 0; i < BLOCK_NUM_W; i++) {
     for (let j = 0; j < BLOCK_NUM_H; j++){
       if (tbl[i][j] == -1) {
         noFill();
       } else {
-        fill(color_block[tbl[i][j]%5]); // tblを元に色塗り
+        fill(color_block[tbl[i][j]]); // tblを元に色塗り
       }
       rect(LEFT_MARGIN+i*W_BLOCK, TOP_MARGIN+j*H_BLOCK, W_BLOCK, H_BLOCK);
 
-      // 下にimos配列
-      noFill();
-      rect(LEFT_MARGIN+i*W_BLOCK, TOP_MARGIN+j*H_BLOCK + 300, W_BLOCK, H_BLOCK);
-      fill(0);
-      text("+1", LEFT_MARGIN+i*W_BLOCK, TOP_MARGIN+j*H_BLOCK + 300, W_BLOCK, H_BLOCK);
-
-      if (LEFT_MARGIN+i*W_BLOCK <= mouseX && mouseX < LEFT_MARGIN+(i+1)*W_BLOCK &&
-      TOP_MARGIN+j*H_BLOCK <= mouseY && mouseY < TOP_MARGIN+(j+1)*H_BLOCK) {
+      // 選択部分をハイライト
+      if (LEFT_MARGIN+i*W_BLOCK <= mouseX &&
+          mouseX < LEFT_MARGIN+(i+1)*W_BLOCK &&
+          TOP_MARGIN+j*H_BLOCK <= mouseY &&
+          mouseY < TOP_MARGIN+(j+1)*H_BLOCK) {
         fill(0,0,0, 50);
-        rect(LEFT_MARGIN+i*W_BLOCK, TOP_MARGIN+j*H_BLOCK, W_BLOCK, H_BLOCK); // 選択部分をハイライト
-        if (mouseIsPressed){
-          upper_left = min(mouse_start[0], mouse_end[0]);
-          upper_right = max(mouse_start[0], mouse_end[0]);
-          
-          lower_left = min(mouse_start[1], mouse_end[1]);
-          lower_right = max(mouse_start[1], mouse_end[1]);
-          if (tbl[i][j] == -1 ) {
-            if (over_mouse == false) { // 最初のマウスクリック
-              mouse_start = [i, j];
-            }
-            over_mouse = true;
-          }
-          //tbl[i][j] = 1; // 色塗り
-          mouse_end = [i, j];
-          paintBlock(upper_left, upper_right, lower_left, lower_right, tbl_cache);
-          //print(tbl_cache);
-        }
+        rect(LEFT_MARGIN+i*W_BLOCK, TOP_MARGIN+j*H_BLOCK, W_BLOCK, H_BLOCK);
       }
     }
   }
